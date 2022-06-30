440b84087364b5d86acfe7684f372120e1b89c55
teremato <everosha.animation@gmail.com>
2022-06-30 19:18:33 +0500

    -all

--- src/Components/Pages/Animation/Animation.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Animation = () => {
-    return (
-        <section>
-            <div>Animation</div>
-        </section>
-    )
-}
--- src/Components/Pages/Collection/Collection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Collection = () => {
-    return (
-        <section>
-            <div>Collection</div>
-        </section>
-    )
-}
